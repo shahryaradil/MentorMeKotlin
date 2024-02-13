6cae0a2fb4f8cf4d9940c428f03a5d3013709576
Shahryar Adil Khan <shahryar_adil10@hotmail.com>
2024-02-13 16:08:57 +0500

    Reset Password page

---
+++ AndroidStudioApp/app/src/main/java/com/shahryaradil/i210771/ResetPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.shahryaradil.i210771
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
